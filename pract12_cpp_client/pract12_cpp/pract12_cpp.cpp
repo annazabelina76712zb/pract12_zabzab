4b25f0458877c15ffe01402266c52f756a95740a
annazabelina76712zb <anna.zabelina07@gmail.com>
2026-02-25 21:25:37 +0500

    Add files via upload

---
+++ pract12_cpp_client/pract12_cpp/pract12_cpp.cpp
@@ -0,0 +1,55 @@
+﻿#include <iostream>
+#include <windows.h>
+#include <time.h>
+
+using namespace std;
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+    srand(time(NULL)); 
+
+    cout << "=== ПРИНТЕР ЗАПУЩЕН ===" << endl << endl;
+
+    const char* mName = "Printer";
+    HANDLE hMut = CreateMutexA(NULL, FALSE, mName);
+    if (hMut == NULL) {
+        cout << "Ошибка создания мьютекса!" << endl;
+        return GetLastError();
+    }
+
+    DWORD lastConnect = GetTickCount64();
+
+    while (true) {
+        DWORD WaitMut = WaitForSingleObject(hMut, 1000);
+
+        if (WaitMut == WAIT_OBJECT_0) {
+            lastConnect = GetTickCount64();
+
+            cout << "Идет печать!" << endl;
+
+            int printTime = 5 + rand() % 11;
+
+            Sleep(printTime * 1000);
+
+            if (printTime > 10) {
+                cout << "Ошибка печати!" << endl;
+            }
+
+            cout << "Готово!" << endl << endl;
+
+            ReleaseMutex(hMut);
+        }
+
+        if (GetTickCount64() - lastConnect > 600000) {
+            cout << "Принтер выключен. Простой больше 10 минут" << endl;
+            break;
+        }
+    }
+
+    CloseHandle(hMut);
+
+    cout << "Нажмите Enter для выхода...";
+    cin.get();
+    return 0;
+}
---
+++ pract12_cpp_client/pract12_cpp_client.cpp
@@ -0,0 +1,83 @@
+﻿#include <iostream>
+#include <windows.h>
+
+using namespace std;
+
+void SetProcessPriority(int choice) {
+    HANDLE hProcess = GetCurrentProcess();
+
+    switch (choice) {
+    case 1: 
+        SetPriorityClass(hProcess, IDLE_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Фоновый (низкий)" << endl;
+        break;
+    case 2: 
+        SetPriorityClass(hProcess, BELOW_NORMAL_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Ниже среднего" << endl;
+        break;
+    case 3: 
+        SetPriorityClass(hProcess, NORMAL_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Нормальный" << endl;
+        break;
+    case 4: 
+        SetPriorityClass(hProcess, ABOVE_NORMAL_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Выше среднего" << endl;
+        break;
+    case 5: 
+        SetPriorityClass(hProcess, HIGH_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Высокий" << endl;
+        break;
+    default:
+        SetPriorityClass(hProcess, NORMAL_PRIORITY_CLASS);
+        cout << "Установлен приоритет: Нормальный (по умолчанию)" << endl;
+    }
+}
+
+int main()
+{
+    setlocale(LC_ALL, "Russian");
+
+    cout << "КЛИЕНТ" << endl << endl;
+    cout << "Выберите приоритет процесса:" << endl;
+    cout << "1 - Фоновый (низкий)" << endl;
+    cout << "2 - Ниже среднего" << endl;
+    cout << "3 - Нормальный" << endl;
+    cout << "4 - Выше среднего" << endl;
+    cout << "5 - Высокий" << endl;
+
+
+    int priorityChoice;
+    cin >> priorityChoice;
+    cin.ignore(); 
+    SetProcessPriority(priorityChoice);
+
+    cout << endl << "Клиент запущен!" << endl << endl;
+
+    const char* mName = "Printer";
+    HANDLE hMut = OpenMutexA(MUTEX_ALL_ACCESS, FALSE, mName);
+    if (hMut == NULL) {
+        cout << "Ошибка открытия мьютекса! Принтер не найден." << endl;
+        cout << "Убедитесь, что процесс 'Принтер' запущен." << endl;
+        return GetLastError();
+    }
+
+    cout << "Принтер найден!" << endl;
+    cout << "Нажмите Enter для отправки задания на печать..." << endl;
+    cin.get();
+
+    cout << "Пытаемся захватить принтер..." << endl;
+    WaitForSingleObject(hMut, INFINITE);
+    cout << "Отправлено на печать!" << endl;
+    ReleaseMutex(hMut);
+
+    cout << "Ожидание завершения печати!" << endl;
+    WaitForSingleObject(hMut, INFINITE);
+    cout << "Печать завершена!" << endl;
+    ReleaseMutex(hMut);
+
+    CloseHandle(hMut);
+
+    cout << endl << "Нажмите Enter для выхода...";
+    cin.get();
+    return 0;
+}
